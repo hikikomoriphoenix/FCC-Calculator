1feb9f313ee8c748d9ba19c18030ee67c5e2bc6b
Loremar Marabillas <loremar5858@gmail.com>
2020-04-24 05:04:32 +0800

    Updates

---
+++ precache-manifest.d0305ac60ef87e8ba621c6656194f22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ced6530e8648b280de15d7d49348db",
+    "url": "/FCC-Calculator/index.html"
+  },
+  {
+    "revision": "c353136694a8aed535e5",
+    "url": "/FCC-Calculator/static/css/main.8a7f0d28.chunk.css"
+  },
+  {
+    "revision": "b54ba872dbd1e0878f54",
+    "url": "/FCC-Calculator/static/js/2.0a1287e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FCC-Calculator/static/js/2.0a1287e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c353136694a8aed535e5",
+    "url": "/FCC-Calculator/static/js/main.e7892a78.chunk.js"
+  },
+  {
+    "revision": "9f15ecb939e8aa806949",
+    "url": "/FCC-Calculator/static/js/runtime-main.0531784e.js"
+  }
+]);
